60768305a3a0d6b40e2802dca5312531346ec4d9
Brayan-7 <brianescobedo_205@hotmail.com>
2015-07-24 16:57:07 -0600

    Carpeta donde estan los .js

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express'); 
+var router = express.Router();
+
+var quizController = require('../controllers/quiz_controller');
+
+
+/* GET home page. */
+router.get('/', function(req, res) {
+res.render('index', { title: 'QUIZ' });
+});
+
+router.param('quizId',quizController.load); // autoload
+
+// defino rutas /quizes
+router.get('/quizes', quizController.index); // busqueda pregunta
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/author',quizController.author);
+router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
+router.put( '/quizes/:quizId(\\d+)', quizController.update);
+module.exports = router;
